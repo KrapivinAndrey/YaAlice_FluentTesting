f914ae3e11b34c5f0ff65ca0e4d232072e52fa18
a.krapivin <a.krapivin@kontur.ru>
2022-04-11 23:41:10 +0500

    feat: Сборка мокирования самих запросов

--- alice_fluentcheck/__init__.py
+++ alice_fluentcheck/__init__.py
@@ -1,4 +1,5 @@
 from alice_fluentcheck.AliceIntents import AliceIntent, AliceIntentSlot
 from alice_fluentcheck.AliceEntity import AliceEntity
+from alice_fluentcheck.AliceRequest import AliceRequest
 
 
